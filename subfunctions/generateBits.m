0c4b498e59a617675aa3c91dc93532bf9bf9e63a
Leonard Berresheim <s0556126@htw-berlin.de>
2020-10-24 15:49:02 +0200

    Add generateBits.m

---
+++ subfunctions/generateBits.m
@@ -0,0 +1,10 @@
+%GENERATEBITS generiert eine zufällige Reihe Bits aus
+%   Eingabe: 
+%       nBits       Anzahl der Bits
+%   Ausgabe: 
+%       y           (1 x nBits) Vektor der zufällig generierten Bits
+
+function y = generateBits(nBits)
+    y = logical(randi([0 1], 1, nBits)); %Creates Vector (1 x nBits) of uniformly distributed pseudorandom logical of [0 1] - 
+    stem(y);
+end
